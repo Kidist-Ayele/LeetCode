91c0a2767c5e616c9352fc8b5c93e0449c8e9097
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2024-10-12 13:28:27 +0300

    Time: 222 ms (58.34%) | Memory: 17.3 MB (97.11%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,22 +1,18 @@
-from typing import List
-
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        total = right = left = cnt = 0
-        prefix_sum = {0: 1}
-        
-        while right < len(nums):
-            total += nums[right] 
-            # Check if there exists a subarray with the required sum
-            if total - goal in prefix_sum:
-                cnt += prefix_sum[total - goal]  # Increment count by the number of times (total - goal) has been seen
-            
-            # Update the frequency of the current prefix sum in the dictionary
-            if total in prefix_sum:
-                prefix_sum[total] += 1
-            else:
-                prefix_sum[total] = 1
+        def helper(x):
+            # If x is negative, no valid subarray can have a sum less than 0
+            if x < 0:
+                return 0
+            res = left = cur = 0
+            for right in range(len(nums)):
+                cur += nums[right]
+                # Shrink the window while the current sum is greater than x
+                while cur > x :
+                    cur -= nums[left]
+                    left += 1
+                # Count the number of subarrays from left to right
+                res += (right - left + 1)
+            return res
 
-            right += 1  # Move the right pointer forward to expand the window
-        
-        return cnt  # Return the number of subarrays with the sum equal to the goal
+        return helper(goal) - helper(goal - 1)
