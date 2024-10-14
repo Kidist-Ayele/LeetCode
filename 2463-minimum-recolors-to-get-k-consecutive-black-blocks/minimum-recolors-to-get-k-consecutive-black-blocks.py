60cb90f6917d9fe3e0217cb470c86bf70f8d7085
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2024-10-14 13:53:23 +0300

    Time: 41 ms (23.96%) | Memory: 16.7 MB (12.82%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,23 +1,18 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         left = 0
-        # Count the number of 'W's on the first window
-        first_window = Counter(blocks[:k])
-        cur_min = first_window['W']
-        cnt_W = first_window['W']
-        n= len(blocks)
-        # Iterate through blocks with a sliding window of size k
-        while left < n - k:
-            # Check if the new character entering the window is 'W'
-            if blocks[left + k] == 'W':
-                cnt_W += 1
-            # Check if the character leaving the window is 'W'
+        cnt_w = blocks[:k].count('W')
+        res = cnt_w
+
+        for right in range(k, len(blocks)):
+            if blocks[right] == 'W':
+                cnt_w += 1
             if blocks[left] == 'W':
-                cnt_W -= 1
-            cur_min = min(cur_min, cnt_W)
+                cnt_w -= 1
             left += 1
-        return cur_min
-            
+            res = min(res, cnt_w)
+        return res
+
             
 
         
