f0a085f691f57654b282c6b66153a1bdf1ccbb70
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2024-12-04 23:03:05 +0300

    Time: 243 ms (34.82%) | Memory: 17.5 MB (5.35%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        left = right = 0 
-        n = len(answerKey)
-        res = 0  
-        max_t = max_f = 0  
-        my_dict = {char:0 for char in "TF"}
+        def max_with_char(cur_char, k):
+            left = 0
+            ans = 0
+            for right in range(len(answerKey)):
+                if answerKey[right] != cur_char:
+                    k -= 1
+                while k < 0:
+                    if answerKey[left] != cur_char:
+                        k += 1
+                    left += 1
+                
+                ans = max(ans, right - left + 1)
+            return ans
+        return max(max_with_char('T', k), max_with_char('F', k))
 
-        for right in range(len(answerKey)):
-            my_dict[answerKey[right]] += 1
-            
-            while min(my_dict['T'], my_dict['F']) > k:
-                my_dict[answerKey[left]] -= 1
-                left += 1
-            res = max(res, right - left + 1)
-        return res
-       
+
+
+        
