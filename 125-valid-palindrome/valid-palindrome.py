b3728196188ae79672311af5a671bfa714b27d22
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2024-12-31 19:54:56 +0300

    Time: 12 ms (19.24%) | Memory: 23.3 MB (5.27%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        new_s = ""
-        
+        s.lower()
+        arr = []
+
+        if len(s) == 0:
+            return True
         for char in s:
             char = char.lower()
-            if char in letters or char.isdigit():
-                new_s += char
-        left, right = 0, len(new_s) - 1
+            if char.isalpha() or char.isdigit():
+                arr.append(char)
+       
+        right = len(arr) - 1
+        left = 0
+
         while left <= right:
-            if new_s[left] != new_s[right]:
+            if arr[left] != arr[right]:
                 return False
-            left += 1
+            
             right -= 1
+            left += 1
+    
         return True
-
-        
-
-        
         
